c80c885d1f6494b13a144413e85728acec03c889
Gustavo Oliveira <gustavo.ramosue@gmail.com>
2023-03-07 23:15:19 -0300

    Add project files.

---
+++ ParImparRetorno/Program.cs
@@ -0,0 +1,25 @@
+﻿int numero;
+Console.WriteLine("Informe um valor para verificar se é par ");
+numero = int.Parse(Console.ReadLine());
+
+if (ehPar(numero))
+{
+    Console.WriteLine($"O número {numero} é par");
+}
+else
+{
+    Console.WriteLine($"O número {numero} é impar");
+}
+
+
+bool ehPar(int valor)
+{
+    if (valor % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
